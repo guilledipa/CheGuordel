f4ab37cdc6814b4a04c660fdae00451b6d54b234
guilledipa <guillermodp@google.com>
2024-10-07 00:41:31 +1100

    Add more improvements. Issue with 'Ñ'

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"bytes"
 	_ "embed"
+	"encoding/json"
 	"fmt"
 	"image/color"
+	"io"
 	"log"
+	"os"
 	"strings"
 	"time"
 	"unicode"
+	"unicode/utf8"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -19,11 +23,21 @@ import (
 )
 
 var (
-	robotoFaceSource *text.GoTextFaceSource
+	fontFaceSource *text.GoTextFaceSource
+	validWords     map[string]any
 )
 
-//go:embed fonts/Roboto-Regular.ttf
-var robotoRegularTTF []byte
+//go:embed fonts/NotoSans-Regular.ttf
+var notoSansRegularTTF []byte
+
+// Mode defines the current game mode.
+type Mode int
+
+const (
+	// modeTitle  Mode = iota
+	modeGame Mode = iota
+	modeGameOver
+)
 
 const (
 	screenWidth    = 800
@@ -37,7 +51,7 @@ const (
 
 // Game implements ebiten.Game interface
 type Game struct {
-	// Do stuff, e.g. game state vars: current word, guesses, etc.
+	mode       Mode
 	targetWord string
 	guesses    []string
 	currentRow int
@@ -47,32 +61,47 @@ type Game struct {
 
 // Update does stuff..
 func (g *Game) Update() error {
-	// Handle inputs, game logic, etc
-	if g.gameWon {
-		return nil // Game over, stop processing inputs
-	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
-		g.handleEnter()
-	} else if inpututil.IsKeyJustPressed(ebiten.KeyBackspace) {
-		g.handleBackspace()
-	} else {
-		for _, r := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
-			// Calculate the correct Ebiten key code for the letter
-			key := ebiten.KeyA + ebiten.Key(unicode.ToUpper(r)-'A')
-			if inpututil.IsKeyJustPressed(key) {
-				fmt.Println(rune(r))
-				g.handleLetterInput(rune(r))
+	switch g.mode {
+	case modeGame:
+		if g.gameWon {
+			return nil // Game over, stop processing inputs
+		}
+		if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
+			g.handleEnter()
+		} else if inpututil.IsKeyJustPressed(ebiten.KeyBackspace) {
+			g.handleBackspace()
+		} else {
+			if inpututil.IsKeyJustPressed(ebiten.KeySemicolon) {
+				g.handleLetterInput('Ñ')
 				break
 			}
+			for _, r := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
+				// Calculate the correct Ebiten key code for the letter
+				key := ebiten.KeyA + ebiten.Key(unicode.ToUpper(r)-'A')
+				if inpututil.IsKeyJustPressed(key) {
+					g.handleLetterInput(rune(r))
+					break
+				}
+			}
 		}
+		// Check if we've already reached the maximum guesses
+		if g.currentRow == maxGuesses {
+			g.mode = modeGameOver
+		}
+	case modeGameOver:
+		// Not implemented yet.
+		return nil
 	}
 	return nil
 }
 
 // Draw draws the game screen and is called after every update.
 func (g *Game) Draw(screen *ebiten.Image) {
+	if g.mode == modeGameOver {
+		ebitenutil.DebugPrint(screen, fmt.Sprintf("Perdiste... La palabra era: %s", g.targetWord))
+	}
 	f := &text.GoTextFace{
-		Source: robotoFaceSource,
+		Source: fontFaceSource,
 		Size:   normalFontSize,
 	}
 	// Draw grid
@@ -82,17 +111,18 @@ func (g *Game) Draw(screen *ebiten.Image) {
 			y := row*tileSize + (row+1)*tileSpacing
 			// Draw tile background
 			tileColor := color.Color(color.White)
-			// Only color tiles in completed rows or the current row
-			if row < g.currentRow || (row == g.currentRow && col < len(g.guesses[row])) {
+			// Only color tiles in completed rows
+			if row < g.currentRow || g.gameWon {
 				tileColor = g.getTileColor(row, col)
 			}
 			// Draw tile
 			vector.DrawFilledRect(screen, float32(x), float32(y), float32(tileSize), float32(tileSize), tileColor, false)
 			// Draw letter
 			if row <= g.currentRow && col < len(g.guesses[row]) {
-				letter := g.guesses[row][col]
+				r, _ := utf8.DecodeRuneInString(g.guesses[row][col:]) // Decode the rune
+				letter := fmt.Sprintf("%c", r)
 				// Calculate text bounds to get width and height
-				textWidth, textHeight := text.Measure(string(letter), f, 0)
+				textWidth, textHeight := text.Measure(letter, f, 0)
 				// Calculate position to center the letter within the tile
 				textX := float64(x) + (float64(tileSize)-textWidth)/2
 				textY := float64(y) + (float64(tileSize)-textHeight)/2
@@ -100,15 +130,14 @@ func (g *Game) Draw(screen *ebiten.Image) {
 				op := &text.DrawOptions{}
 				op.GeoM.Translate(textX, textY)
 				op.ColorScale.ScaleWithColor(color.Black)
-				text.Draw(screen, string(letter), f, op)
+				text.Draw(screen, letter, f, op)
+				ebitenutil.DebugPrint(screen, letter)
 			}
 		}
 	}
 	// Display game over message
 	if g.gameWon {
 		ebitenutil.DebugPrint(screen, "Ganaste!")
-	} else if g.currentRow == maxGuesses {
-		ebitenutil.DebugPrint(screen, fmt.Sprintf("Perdiste... La palabra era: %s", g.targetWord))
 	}
 }
 
@@ -118,6 +147,23 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func (g *Game) handleEnter() error {
+	// Do nothing if current guess word length is is not equal to wordLength
+	if len(g.guesses[g.currentRow]) != wordLength {
+		return nil
+	}
+
+	// Check if the word is in the valid word list
+	if !isValidWord(strings.ToLower(g.guesses[g.currentRow])) {
+		// Handle invalid word (e.g., display an error message)
+		fmt.Printf("%q no es una palabra válida.\n", g.guesses[g.currentRow])
+		return nil
+	}
+	// Check if the guess is correct
+	if g.guesses[g.currentRow] == g.targetWord {
+		g.gameWon = true
+		return nil
+	}
+	// Go to next row if the guess is incorrect
 	g.currentRow++
 	return nil
 }
@@ -130,9 +176,12 @@ func (g *Game) handleBackspace() error {
 }
 
 func (g *Game) handleLetterInput(r rune) error {
-	fmt.Println(string(r))
 	if len(g.guesses[g.currentRow]) < wordLength {
-		g.guesses[g.currentRow] = g.guesses[g.currentRow] + strings.ToUpper(string(r))
+		var sb strings.Builder
+		sb.WriteString(g.guesses[g.currentRow]) // Add the existing string
+		sb.WriteRune(r)                         // Add the rune (handles multi-byte correctly)
+		g.guesses[g.currentRow] = sb.String()
+		fmt.Println(g.guesses[g.currentRow])
 	}
 	return nil
 }
@@ -149,20 +198,52 @@ func (g *Game) getTileColor(row, col int) color.Color {
 	return color.RGBA{128, 128, 128, 255} // Gray
 }
 
+// Check if a given word is valid (UPdate)
+func isValidWord(word string) bool {
+	if _, ok := validWords[word]; ok {
+		return true
+	}
+	return false
+}
+
+// randomWord returns a random validGuesses key.
+// This approach relies on the semantics of range; range will visit every
+// key/value in the map exactly once in **unspecified** order.
+// I'll use this as a way of "randomly" choose a target word.
+func randomWord(m map[string]any) string {
+	rand.Seed(uint64(time.Now().UnixNano()))
+	r := rand.Intn(len(m))
+	for k := range m {
+		if r == 0 {
+			return k
+		}
+		r--
+	}
+	panic("randomWord: unreachable point.")
+}
+
 func init() {
 	// Load font
-	s, err := text.NewGoTextFaceSource(bytes.NewReader(robotoRegularTTF))
+	var err error
+	fontFaceSource, err = text.NewGoTextFaceSource(bytes.NewReader(notoSansRegularTTF))
 	if err != nil {
 		log.Fatal(err)
 	}
-	robotoFaceSource = s
+	// Load word list
+	jsonFile, err := os.Open("constants/validGuesses.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer jsonFile.Close()
+	bytes, err := io.ReadAll(jsonFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.Unmarshal([]byte(bytes), &validWords)
 }
 
 func main() {
-	// Choose a random word (Temporal)
-	wordList := []string{"MESSI", "CHETO", "CHINO", "PUCHO", "BOLSA"}
-	rand.Seed(uint64(time.Now().UnixNano()))
-	targetWord := wordList[rand.Intn(len(wordList))]
+	targetWord := strings.ToUpper(randomWord(validWords))
 	fmt.Println(targetWord)
 	// Initialize game
 	g := &Game{
